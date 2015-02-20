61871f3509b1e744bc334f300216c402fd918ba0
derek1906 <derek1906@gmail.com>
2015-02-20 14:02:03 -0600

    Intital

--- demo.cpp
+++ demo.cpp
@@ -7,17 +7,17 @@ class List{
     class ListNode{
         public: 
         ListNode(int d){
-			data = d;
-			next = NULL;
-		}
-		ListNode* next;
-		int data;
+            data = d;
+            next = NULL;
+        }
+        ListNode* next;
+        int data;
     };
     
     public:
     List(){
-		head = NULL;
-	};
+        head = NULL;
+    };
     
     void print(ostream& s){
         ListNode** curr = &head;
@@ -34,39 +34,38 @@ class List{
         while(*curr){
             curr = &(*curr)->next;
         }
-		*curr = new ListNode(d);
+        *curr = new ListNode(d);
     }
 
-	ListNode** find(int offset){
-		return find(head, offset);
-	}
+    ListNode** find(int offset){
+        return find(head, offset);
+    }
 
-	ListNode** find(ListNode* &link, int offset){
-		return !link ? NULL : offset ? find(link->next, --offset) : &link;
-	}
+    ListNode** find(ListNode* &link, int offset){
+        return !link ? NULL : offset ? find(link->next, --offset) : &link;
+    }
 
-	void insertAt(int offset, int d){
-		ListNode** node = find(offset);
-		ListNode* newN = new ListNode(d);
-		newN->next = *node;
-		*node = newN;
-	}
+    void insertAt(int offset, int d){
+        ListNode** node = find(offset);
+        ListNode* newN = new ListNode(d);
+        newN->next = *node;
+        *node = newN;
+    }
     
     ListNode* head;
 };
 
-int main()
-{
-  List l;
-  l.insertBack(0);
-  l.insertBack(1);
-  l.insertBack(2);
-  l.insertBack(3);
-  l.insertBack(4);
-  l.insertBack(5);
-  l.print(cout);
-  cout << (*l.find(2))->data << endl;
-  l.insertAt(3, 10);
-  l.insertAt(0, 100);
-  l.print(cout);
+int main(){
+    List l;
+    l.insertBack(0);
+    l.insertBack(1);
+    l.insertBack(2);
+    l.insertBack(3);
+    l.insertBack(4);
+    l.insertBack(5);
+    l.print(cout);
+    cout << (*l.find(2))->data << endl;
+    l.insertAt(3, 10);
+    l.insertAt(0, 100);
+    l.print(cout);
 }
